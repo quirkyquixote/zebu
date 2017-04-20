07a78ff948cff77b7898d7803cfd072e42d4843a
Luis Sanz <luis.sanz@gmail.com>
2017-04-20 20:23:00 +0200

    Fix installation problems
    Fix recipes for *.so and *.a in config.mk so they don't require an
    object file of the same name.
    
    Actually install header files as part of make install invocation.
    
    Add *.a to .gitignore.

--- config.mk
+++ config.mk
@@ -6,6 +6,7 @@ RM = rm -f
 version = 1.0
 prefix = /usr/local
 libdir = $(prefix)/lib
+includedir = $(prefix)/include/zebu
 
 CPPFLAGS =
 
@@ -33,10 +34,10 @@ QUIET_GEN = @echo GEN $@;
 %.o: %.c
 	$(QUIET_CC)$(CC) $(ALL_CFLAGS) -c $<
 
-lib%.so: %.o
+lib%.so:
 	$(QUIET_LINK)$(CC) -shared -Wl,-soname,$@.$(version) -o $@ $^
 
-lib%.a: %.o
+lib%.a:
 	$(QUIET_AR)$(AR) rcs $@ $^
 
 $(DESTDIR)$(libdir)/%.so: %.so
@@ -45,5 +46,9 @@ $(DESTDIR)$(libdir)/%.so: %.so
 	@cd $(DESTDIR)$(libdir) && ln -s -f $<.$(version) $<
 
 $(DESTDIR)$(libdir)/%.a: %.a
+	@$(INSTALL) -d $(@D)
+	$(QUIET_INSTALL)$(INSTALL) $< $@
+
+$(DESTDIR)$(includedir)/%.h: %.h
 	@$(INSTALL) -d $(@D)
 	$(QUIET_INSTALL)$(INSTALL) $< $@
