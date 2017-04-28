83a0285d22a38e0d35908879215d4cf7c9cfef3d
Luis Sanz <luis.sanz@gmail.com>
2017-04-28 22:23:17 +0200

    Dict are ref. counted; add lookup, delete functions
    Each zz_dict object has now a reference counter to determine how many
    times the string has been added to the dictionary.
    
    The zz_dict_delete function can now be used to remove entries from a
    dictionary. The strings are actually deallocated when the function is
    called as many times as the zz_dict_insert was.
    
    The zz_dict_lookup function can be used to retrieve a string without
    incrementing its reference counter.

--- src/dict.c
+++ src/dict.c
@@ -5,7 +5,16 @@
 #include <string.h>
 #include <unistd.h>
 
-static struct zz_dict *zz_dict_skew(struct zz_dict *t)
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define SWAP(a, b)\
+do {\
+	typeof(a) tmp = a;\
+	a = b;\
+	b = tmp;\
+} while (0)
+
+static struct zz_dict *skew(struct zz_dict *t)
 {
 	struct zz_dict *l;
 
@@ -23,7 +32,7 @@ static struct zz_dict *zz_dict_skew(struct zz_dict *t)
 	}
 }
 
-static struct zz_dict *zz_dict_split(struct zz_dict *t)
+static struct zz_dict *split(struct zz_dict *t)
 {
 	struct zz_dict *r;
 
@@ -42,15 +51,62 @@ static struct zz_dict *zz_dict_split(struct zz_dict *t)
 	}
 }
 
+static struct zz_dict *sucessor(struct zz_dict *t)
+{
+	for (t = t->right; t->left; t = t->left)
+		continue;
+	return t;
+}
+
+static struct zz_dict *predecessor(struct zz_dict *t)
+{
+	for (t = t->left; t->right; t = t->right)
+		continue;
+	return t;
+}
+
+static struct zz_dict *decrease_level(struct zz_dict *t)
+{
+	int right_level = t->right ? t->right->level : 0;
+	int left_level = t->left ? t->left->level : 0;
+	int should_be = MIN(left_level, right_level) + 1;
+	if (should_be < t->level) {
+		t->level = should_be;
+		if (should_be < right_level)
+			t->right->level = should_be;
+	}
+	return t;
+}
+
+int zz_dict_lookup(struct zz_dict *t, const char *data, const char **rval)
+{
+	int cmp;
+
+	while (t != NULL) {
+		cmp = strcmp(data, t->data);
+		if (cmp < 0) {
+			t = t->left;
+		} else if (cmp > 0) {
+			t = t->right;
+		} else {
+			if (rval != NULL)
+				*rval = t->data;
+			return 1;
+		}
+	}
+	return 0;
+}
+
 struct zz_dict *zz_dict_insert(struct zz_dict *t, const char *data,
 		const char **rval)
 {
 	int cmp;
 
 	if (t == NULL) {
-		t = calloc(1, sizeof(*t) + strlen(data) + 1);
+		t = calloc(1, sizeof(*t));
 		t->level = 1;
-		strcpy(t->data, data);
+		t->ref_count = 1;
+		t->data = strdup(data);
 		if (rval != NULL)
 			*rval = t->data;
 		return t;
@@ -61,11 +117,58 @@ struct zz_dict *zz_dict_insert(struct zz_dict *t, const char *data,
 	} else if (cmp > 0) {
 		t->right = zz_dict_insert(t->right, data, rval);
 	} else {
+		++t->ref_count;
 		if (rval != NULL)
 			*rval = t->data;
 	}
-	t = zz_dict_skew(t);
-	t = zz_dict_split(t);
+	t = skew(t);
+	t = split(t);
+	return t;
+}
+
+struct zz_dict *zz_dict_delete(struct zz_dict *t, const char *data)
+{
+	int cmp;
+
+	if (t == NULL)
+		return t;
+	cmp = strcmp(data, t->data);
+	if (cmp > 0) {
+		t->right = zz_dict_delete(t->right, data);
+	} else if (cmp < 0) {
+		t->left = zz_dict_delete(t->left, data);
+	} else {
+		if (t->ref_count > 1) {
+			--t->ref_count;
+			return t;
+		}
+		if (t->left == NULL) {
+			if (t->right == NULL) {
+				free(t->data);
+				free(t);
+				return NULL;
+			}
+			struct zz_dict *l = sucessor(t);
+			SWAP(t->data, l->data);
+			SWAP(t->ref_count, l->ref_count);
+			t->right = zz_dict_delete(t->right, l->data);
+		} else {
+			struct zz_dict *l = predecessor(t);
+			SWAP(t->data, l->data);
+			SWAP(t->ref_count, l->ref_count);
+			t->left = zz_dict_delete(t->left, l->data);
+		}
+	}
+
+	/* Rebalance the tree. Decrease the level of all nodes in this level if
+	 * necessary, and then skew and split all nodes in the new level. */
+	t = decrease_level(t);
+	t = skew(t);
+	t->right = skew(t->right);
+	if (t->right != NULL)
+		t->right->right = skew(t->right->right);
+	t = split(t);
+	t->right = split(t->right);
 	return t;
 }
 
@@ -74,6 +177,7 @@ void zz_dict_destroy(struct zz_dict *t)
 	if (t != NULL) {
 		zz_dict_destroy(t->left);
 		zz_dict_destroy(t->right);
+		free(t->data);
 		free(t);
 	}
 }
--- src/dict.h
+++ src/dict.h
@@ -11,22 +11,26 @@
  * binary tree. We use an even more simplified form of it to keep an easily
  * accessible index of all strings belonging to an AST.
  *
- * This implementation lacks specialized lookup and deletion routines: lookup
- * is provided by the insert method--attempting to insert an already existing
- * string will return the already allocated data--, and elements can't be
- * deleted individually, but are destroyed all at once with the AST they belong
- * to.
+ * This implementation lacks specialized lookup routines: lookup is provided by
+ * the insert method--attempting to insert an already existing string will
+ * return the already allocated data.
  */
 struct zz_dict {
 	/* Left and right children */
 	struct zz_dict *left, *right;
 	/* Level of this node */
 	size_t level;
+	/* Reference counter */
+	size_t ref_count;
 	/* Data attached to this node */
-	char data[];
+	char *data;
 };
 
+int zz_dict_lookup(struct zz_dict *t, const char *data, const char **rval);
+
 struct zz_dict *zz_dict_insert(struct zz_dict *t, const char *data, const char **rval);
+
+struct zz_dict *zz_dict_delete(struct zz_dict *t, const char *data);
 
 void zz_dict_destroy(struct zz_dict *t);
 
